cb8592a53d8c3381d5c7591c047a711b02e34d5e
vundi4773 <vundi4773@gmail.com>
2024-05-04 12:23:07 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail){
+const postedData = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name:'Jackson',
+    email:'Jay@gmail.com',
+  }),
+};
+
+fetch("http://localhost:3000/users", postedData)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch(function(error){
+    console.log('error posting data')
+  });
+return fetch
+}
